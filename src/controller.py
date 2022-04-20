bbf564e537f0b3958fd1342afc6dca4813c48be3
briancsavage <47962267+briancsavage@users.noreply.github.com>
2022-04-20 08:53:55 -0400

    Refactoring for controller file

--- src/controller.py
+++ src/controller.py
@@ -1,54 +1,21 @@
 import numpy as np
 from djitellopy import Tello
-from typing import Tuple
+from typing import Tuple, Dict, Any
+from dataclasses import dataclass, field
 
 
-class Controller:
-    """
-        Wrapper class for DJITelloPy Module to fetch sensing data and
-        execute instructions for the Tello drone.
+@dataclass
+class Control:
+    tello: Tello = Tello()
+    inAir: bool = False
+    maxMove: int = 30
+    maxRotation: int = 30
+    operations: Dict[str, Any] = field(default_factory=dict)
 
-    """
     def __init__(self):
-        self.tello = Tello()
-        # self.tello.connect()
-        # self.tello.streamon()
-
-        self.inAir = False
-        self.maxMagnitudeMove = 10
-        self.maxMagnitudeRotation = 30
-
-    def center_in_view(self,
-                       image: np.ndarray,
-                       x: int,
-                       y: int) -> Tuple[Tuple[str, int], Tuple[str, int]]:
-        H, W = image.shape[:2]
-        mX = "counter_clockwise" if x < W // 2 else "clockwise"
-        mY = "up" if y < H // 2 else "down"
-        dX = (abs((W // 2) - x) / (W // 2)) * self.maxMagnitudeRotation
-        dY = (abs((H // 2) - y) / (H // 2)) * self.maxMagnitudeMove
-        return (mX, dX), (mY, dY)
-
-    def takeoff(self):
-        if not self.inAir:
-            try:
-                self.tello.takeoff()
-                self.inAir = True
-            except Exception as err:
-                print(f"Error raised during takeoff: {err}")
-
-    def land(self):
-        if self.inAir:
-            try:
-                self.tello.land()
-                self.inAir = False
-            except Exception as err:
-                print(f"Error raised during landing: {err}")
-        else:
-            print("Drone hasn't taken off. Can't execute land instruction.")
-
-    def move(self, direction: str, magnitude: int):
-        cases = {
+        self.tello.connect()
+        self.tello.streamon()
+        self.operations = {
             "forward": self.tello.move_forward,
             "back": self.tello.move_back,
             "left": self.tello.move_left,
@@ -59,20 +26,89 @@ class Controller:
             "counter_clockwise": self.tello.rotate_counter_clockwise,
             "none": lambda _: None
         }
+
+    def center_in_view(self,
+                       image: np.ndarray,
+                       x: int,
+                       y: int) -> Tuple[Tuple[str, int], Tuple[str, int]]:
+        """
+            @Does
+
+            @Params
+
+            @Returns
+
+        """
+        H, W = image.shape[:2]
+        mX = "counter_clockwise" if x < W // 2 else "clockwise"
+        mY = "up" if y < H // 2 else "down"
+        dX = (abs((W // 2) - x) / (W // 2)) * self.maxRotation
+        dY = (abs((H // 2) - y) / (H // 2)) * self.maxMove
+        return (mX, dX), (mY, dY)
+
+    def takeoff(self):
+        """
+            @Does
+
+            @Params
+
+            @Returns
+
+        """
         if self.inAir:
-            if direction in cases:
-                if 0 < magnitude < 100:
-                    try:
-                        return cases[direction](magnitude)
-                    except Exception as err:
-                        print(f"Error raised for instruction "
-                              f"({direction}, {magnitude}): {err}")
-                else:
-                    print(f"Invalid magnitude: {magnitude}")
-            else:
-                print(f"Invalid Direction: {direction}")
-        else:
+            print("Drone is already in air.")
+            return self
+        try:
+            self.tello.land()
+            self.inAir = False
+            return self
+        except Exception as err:  # TODO: Add specific exception handling
+            print(f"Error raised during landing: {err}")
+
+    def land(self):
+        """
+            @Does
+
+            @Params
+
+            @Returns
+
+        """
+        if not self.inAir:
+            print("Drone hasn't taken off. Can't execute land instruction.")
+        try:
+            self.tello.land()
+            self.inAir = False
+            return self
+        except Exception as err:
+            print(f"Error raised during landing: {err}")
+
+    def move(self, direction: str, magnitude: int):
+        """
+            @Does
+
+            @Params
+
+            @Returns
+
+        """
+        if not self.inAir:
             print("Drone hasn't taken off. Can't execute move instruction.")
+            return self
+
+        if direction not in self.operations:
+            print(f"Invalid direction: {direction}")
+            return self
+
+        if 0 <= magnitude <= self.maxMove:
+            try:
+                self.operations[direction](magnitude)
+                return self
+            except Exception as err:
+                print(f"Error raised for instruction "
+                      f"({direction}, {magnitude}): {err}")
+        else:
+            print(f"Invalid magnitude: {magnitude}")
 
     def view(self):
         return self.tello.get_frame_read().frame
